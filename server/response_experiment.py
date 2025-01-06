45484a6ebd80ee9db1346cef90d78c999e2c513f
jo49973477 <jo49973477@naver.com>
2025-01-06 13:29:54 +0100

    dockerfile add

--- server/response_experiment.py
+++ server/response_experiment.py
@@ -1,9 +1,11 @@
 import requests
 import torch
-import numpy
+import numpy as np
 
 url = "http://localhost:5000/process-image"
 files = {'image': open('blackbox.jpg', 'rb')}
 response = requests.post(url, files=files).json()
 
 print("detections: ", response["detections"])
+print("drivable area: ", np.array(response["segmentation"]["drivable_area"]))
+print("lane_lines: ",  np.array(response["segmentation"]["lane_lines"]))
